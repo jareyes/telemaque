51397d13cd9ddf7301c467e4b90471e6bdc1c819
Josh Reyes <josh@joshreyes.com>
2025-12-26 23:44:35 -0500

    Use Git LFS for vendor files

--- vendor/onnxruntime-web@1.18.0/ort-wasm-simd-threaded.worker.js
+++ vendor/onnxruntime-web@1.18.0/ort-wasm-simd-threaded.worker.js
@@ -1,1 +1,3 @@
-Couldn't find the requested file /dist/ort-wasm-simd-threaded.worker.js in onnxruntime-web.
+version https://git-lfs.github.com/spec/v1
+oid sha256:496ccc7eb1bce3ee696ea0a6e3fea52d6a073f9d788e8f2e998649c82cd78d58
+size 91
--- vendor/onnxruntime-web@1.18.0/ort-wasm-threaded.worker.js
+++ vendor/onnxruntime-web@1.18.0/ort-wasm-threaded.worker.js
@@ -1,1 +1,3 @@
-Couldn't find the requested file /dist/ort-wasm-threaded.worker.js in onnxruntime-web.
+version https://git-lfs.github.com/spec/v1
+oid sha256:cf5b65a80c8356a2a1c6054dc13f8382ac16daf84eaf3dc478a469140e685cd4
+size 86
